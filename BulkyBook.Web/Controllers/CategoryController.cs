09dffa658926fc145ba0cd2fecd6a45c4ddeb78c
Hoan <Hoan@DESKTOP-M6T0Q7V>
2022-04-18 21:02:43 +0700

    Add project files.

---
+++ BulkyBook.Web/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using BulkyBook.Web.Data;
+using BulkyBook.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Category> Categories { get; set; }
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+            Categories = new List<Category>();
+        }
+
+        public IActionResult Index()
+        {
+            Categories = _db.Categories;
+            return View(this);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var category = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ BulkyBook.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BulkyBook.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
